baae2e27515da6325ace9563054e61e3e98e1424
vasudha-git02 <104597335+vasudha-git02@users.noreply.github.com>
2022-09-29 20:24:35 -0400

    Update join.sql

--- join.sql
+++ join.sql
@@ -5,10 +5,16 @@ SELECT * FROM RECIDIVISM
 SELECT * FROM OFFENSE
 
 
-SELECT p."Year Released" ,p."Age",
-o."Offense Type"
+SELECT p."Year Released" ,p."Age",p."Release Type",p."Race",p."Sex",
+o."Offense Type",o."Offense Classification",o."Offense Subtype",
+r."Days to Return",r."Recidivism Type",r."New Offense Classification",
+r."New Offense Type",r."New Offense Sub Type",r."Target Population",
+r."Return to Prison"
 from PRISONERS p
 INNER JOIN
 OFFENSE o
 on p."ID" = o."ID"
+INNER JOIN
+RECIDIVISM r
+on p."ID" = r."ID"
 where o."Offense Type" != 'Other';
